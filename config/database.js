521f262d99c40d8a03a52d265612b22de926d11c
dev-satendra <satendrashaky1993@gmail.com>
2018-02-14 23:22:42 +0530

    database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-	module.exports = { dburl : 'mongodb://sandy:sandy@ds235418.mlab.com:35418/vidjot' };
+	module.exports = { dburl : 'mongodb://localhost:27017/vidjot' };
 }else{
 	module.exports = { dburl : 'mongodb://localhost:27017/vidjot' }; 	
 }
